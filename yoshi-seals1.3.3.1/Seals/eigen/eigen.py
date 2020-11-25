5f5add0710a76141ea3c64c1c517675e3c833b19
Vitor Hideyoshi <vitor.h.n.batista@gmail.com>
2020-11-25 07:46:46 -0300

    v1.3.3.1

---
+++ yoshi-seals1.3.3.1/Seals/eigen/eigen.py
@@ -0,0 +1,64 @@
+# Seals - Program made for educational intent, can be freely distributed
+# and can be used for economical intent. I will not take legal actions
+# unless my intelectual propperty, the code, is stolen or change without permission.  
+
+# Copyright (C) 2020  VItor Hideyoshi Nakazone Batista
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as published by
+# the Free Software Foundation.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+import numpy as np
+
+def eigen(a):
+
+    k = 0
+    l = np.ones((a.shape[0]))
+
+    at = a #variavel temporaria para A
+    b = np.random.rand(a.shape[0],a.shape[1])
+
+    while (k < at.shape[0]):
+
+        u = np.random.rand(at.shape[0],1)
+        u = u/max(u.min(), u.max(), key=abs)
+
+        ctrl = 0
+
+        while (ctrl != l[k]):
+            
+            ctrl = l[k]
+            u = at.dot(u)
+            l[k] = max(u.min(), u.max(), key=abs)
+            u = u/l[k]
+
+        alpha = 0.999*l[k]
+
+        t = np.random.rand(a.shape[0],1)
+
+        b[k] = b[k]/max(b[k].min(), b[k].max(), key=abs)
+        t = l/max(l.min(), l.max(), key=abs)
+
+        while not (np.allclose(b[k],t,atol=10**(-17))):
+            t = b[k].copy()
+            b[k] = np.linalg.solve((a - alpha*np.identity(a.shape[0])),((l[k]-alpha)*t))
+            b[k] = b[k]/max(b[k].min(), b[k].max(), key=abs)
+
+        i = 0
+
+        while (u[i] == 0):
+            i += 1
+
+        at = at - (1/u[i])*u*at[i]
+        k += 1
+
+    return l, b
---
+++ yoshi-seals1.3.3.1/Seals/write/write.py
@@ -0,0 +1,33 @@
+# Seals - Program made for educational intent, can be freely distributed
+# and can be used for economical intent. I will not take legal actions
+# unless my intelectual propperty, the code, is stolen or change without permission.  
+
+# Copyright (C) 2020  VItor Hideyoshi Nakazone Batista
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as published by
+# the Free Software Foundation.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+import csv
+
+def numpy(array, path):
+
+    with open(path, mode='w') as sistema_linear:
+
+        solution_writer = csv.writer(sistema_linear, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        solution_writer.writerows(array)
+        
+    return array
+
+def pandas(df, path):
+    
+    df.to_csv(path)
